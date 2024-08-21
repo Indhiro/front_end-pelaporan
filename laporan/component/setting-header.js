67ec3f9380d27a7c5afef3a11948580c8fa86bd6
unknown <benhurchrinstoper@gmail.com>
2024-08-21 18:54:38 +0700

    lot fix

---
+++ laporan/component/setting-header.js
@@ -0,0 +1,32 @@
+let setHeader = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filter</title>
+</head>
+<body>
+    <ul class="settings-menu">
+        <li><a href="settings-profile.html">Profile</a></li>
+        <li class=""><a href="settings-application.html">Security</a></li>
+    <!--     <li class=""><a href="settings-security.html">Application</a></li>
+    <li class=""><a href="settings-activity.html">Activity</a></li>
+    <li class=""><a href="settings-payment-method.html">Payment Method</a></li> -->
+        <li class=""><a href="settings-api.html">User Management</a></li>
+    </ul>
+</body>
+</html>
+`
+
+class SettingHeader extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = setHeader;
+    }
+}
+  
+customElements.define('setheader-component', SettingHeader);
--- laporan/js/filterBar.js
+++ laporan/js/filterBar.js
@@ -1,18 +1,25 @@
-let sortType = '';
+let sortType = 'desc';
 let sortLabel = document.getElementById('sortLabel');
-sortLabel.innerHTML = `Sort By : ${sortType}`
+let filterTemp = 'point';
+sortLabel.innerHTML = `Sort By : ${filterTemp},${sortType}`
 
 function buttonSort(type) {
     let statusValue = document.getElementById('selectedStatus').value;
     let sortLabel = document.getElementById('sortLabel');
-    if (sortType == 'asc') {
-        sortType = 'desc'
-    } else if (sortType == 'desc') {
-        sortType = 'asc'
-    } else {
-        sortType = 'asc'
+    if (type != 'status') {
+        if (sortType == 'asc') {
+            sortType = 'desc'
+        } else if (sortType == 'desc') {
+            sortType = 'asc'
+        } else {
+            sortType = 'asc'
+        }
+        filterTemp = type
     }
-    if (type == 'status') sortLabel.innerHTML += `; ${type}`
-    else sortLabel.innerHTML = `Sort By : ${type},${sortType}`
+    if (type == 'status') {
+        sortLabel.innerHTML = `Sort By : ${filterTemp},${sortType} ; ${type}`
+    } else {
+        sortLabel.innerHTML = `Sort By : ${type},${sortType}`
+    }
     return getLaporan(null, `${type},${sortType}`, type, statusValue)
 }
